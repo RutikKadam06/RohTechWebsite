3b263f2ef0e3715e4ba4097b2478778b23534285
rohan60874 <rohanpawar60874@gmail.com>
2025-10-25 22:10:52 +0530

    change by roh

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,5 +1,8 @@
+import AboutSection from '@/components/about'
+import Contact from '@/components/Contact'
 import HeroSection from '@/components/home'
 import Navbar from '@/components/navbar'
+import ServicesSection from '@/components/services'
 import React from 'react'
 
 const page = () => {
@@ -8,6 +11,9 @@ const page = () => {
         <Navbar />
         <br/><br/><br/>
       <HeroSection />
+      <ServicesSection/>
+      <AboutSection/>
+       <Contact/>
     </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from '../components/navbar'
 import Home from '../components/home'
+import ServicesSection from '@/components/services'
+import AboutSection from '@/components/about'
 
 const page = () => {
   return (
     <div>
       <Navbar/>
       <Home/>
+      <ServicesSection/>
+      <AboutSection/>
     </div>
   )
 }
